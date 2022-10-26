665ab8808a33baa6d55fe414fd41f3134ab580ad
br1lisboa <brlisbo@gmail.com>
2022-10-26 08:54:50 -0300

    finish model chat

---
+++ models/chat-msjs.js
@@ -0,0 +1,43 @@
+class Msg {
+    constructor(uid, name, msg) {
+        this.uid = uid
+        this.name = name
+        this.msg = msg
+    }
+}
+
+class ChatMsjs {
+    constructor() {
+        this.mesagges = []
+        this.users = {}
+    }
+    // Metodos para insertar y controlar de manera controloada mesagges y users
+
+    get last10() {
+        this.mesagges = this.mesagges.splice(0, 10)
+        return this.mesagges
+    }
+
+    get usersArr() {
+        return Object.values(this.users) //> Con este metodo barro todo el objeto y me retornada un arreglo de objetos [{},{},{}]
+    }
+
+    // Metodo para enviar los mensajes, necesito el uid de la persona que esta enviando el msj, name, y el mensaje propiamente
+    sendMsg(uid, name, msg) {
+        this.mesagges.unshift(
+            new Msg(uid, name, msg)
+        )
+    }
+
+    // Metodo para agregar un usuario
+    connectUser(user) {
+        this.users[user.id] = user
+    }
+
+    // Metodo para desconectar usuario
+    disconnectUser(id) {
+        delete this.users[id]
+    }
+}
+
+module.exports = ChatMsjs
--- models/index.js
+++ models/index.js
@@ -5,11 +5,13 @@ const Role = require('./role')
 const Server = require('./server')
 const User = require('./user')
 const Product = require('./product')
+const ChatMsjs = require('./chat-msjs')
 
 module.exports = {
     Category,
     Role,
     Server,
     User,
-    Product
+    Product,
+    ChatMsjs
 }
