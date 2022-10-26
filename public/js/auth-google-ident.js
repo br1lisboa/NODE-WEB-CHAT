aa6fdaae2711535b8f5824a6858d0b9c9fd2fbb8
br1lisboa <brlisbo@gmail.com>
2022-10-26 06:59:31 -0300

    finish login whit google or manual and set token on localStorage

--- public/js/auth-google-ident.js
+++ public/js/auth-google-ident.js
@@ -1,3 +1,41 @@
+// Hacemos la referencia al unico form que tenemos en nuestro html
+const myForm = document.querySelector('form')
+
+
+const url = (window.location.hostname.includes('localhost'))
+    ? 'http://localhost:6060/api/auth/'
+    : 'https://restserver-n0d3js.herokuapp.com/api/auth/'
+
+
+// Le agregamos el listener al form
+myForm.addEventListener('submit', e => {
+    e.preventDefault()
+
+    const formData = {}
+
+    // Leemos todos los datos del formulario
+    for (let e of myForm.elements) {
+        if (e.name.length > 0) formData[e.name] = e.value
+    }
+
+    fetch(url + 'login', {
+        method: 'POST',
+        body: JSON.stringify(formData),
+        headers: { 'Content-type': 'application/json' }
+    })
+        .then(resp => resp.json())
+        .then(({ msg, token }) => {
+            if (msg) {
+                return console.error(msg)
+            }
+            localStorage.setItem('token', token)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+})
+
+
 function handleCredentialResponse(response) {
 
     // GOOGLE token : ID_TOKEN
@@ -5,7 +43,7 @@ function handleCredentialResponse(response) {
     const body = { id_token: response.credential }
 
     // Por defecto el fetch es una peticion GET, nosotros esamos la peticion POST para el sigIn, por lo que hay que transformar esto.
-    fetch('http://localhost:6060/api/auth/google', {
+    fetch(url + 'google', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -13,9 +51,9 @@ function handleCredentialResponse(response) {
         body: JSON.stringify(body)
     })
         .then(resp => resp.json())
-        .then(resp => {
-            console.log(resp)
-            localStorage.setItem('mail', resp.users.mail)
+        .then(({ token }) => {
+            console.log(token)
+            localStorage.setItem('token', token)
         })
         .catch(console.warn)
 }
